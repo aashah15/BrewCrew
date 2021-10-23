20cd7503c4800053b1efe9ba3803be06ed434d1f
Ashraf Shah <ashrafashah@hotmail.com>
2021-10-23 13:57:39 -0600

    Using StreamProvider from Provider package

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,4 @@
 import 'package:brew_crew2/services/auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,12 +1,15 @@
+import 'package:brew_crew2/models/my_user.dart';
 import 'package:brew_crew2/screens/authenticate/authenticate.dart';
-import 'package:brew_crew2/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<MyUser?>(context);
+    print(user);
     // return either Home or Authenticate widget
     return const Authenticate();
   }
