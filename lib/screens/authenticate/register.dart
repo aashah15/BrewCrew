84110a9f336b6d32715321e4cd100069ce1452fd
Ashraf Shah <ashrafashah@hotmail.com>
2021-10-26 01:08:26 -0600

    Register with email and Password

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -11,10 +11,12 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -38,12 +40,14 @@ class _RegisterState extends State<Register> {
       body: Container(
           padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
           child: Form(
+            key: _formKey,
             child: Column(
               children: [
                 const SizedBox(
                   height: 20,
                 ),
                 TextFormField(
+                  validator: (val) => val!.isEmpty ? 'Enter an email' : null,
                   onChanged: (value) {
                     setState(() {
                       email = value;
@@ -54,6 +58,9 @@ class _RegisterState extends State<Register> {
                   height: 20,
                 ),
                 TextFormField(
+                  validator: (val) => val!.isEmpty
+                      ? 'Enter a password atleast 6 chars long'
+                      : null,
                   obscureText: true,
                   onChanged: (value) {
                     setState(() {
@@ -67,10 +74,24 @@ class _RegisterState extends State<Register> {
                 ElevatedButton(
                   child: const Text('Register'),
                   style: ElevatedButton.styleFrom(primary: Colors.brown),
-                  onPressed: () {
-                    print(email);
-                    print(password);
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      dynamic result = await _auth.registerWithEmailAndPassword(
+                          email, password);
+                      if (result == null) {
+                        setState(() {
+                          error = 'Please Enter a valid email';
+                        });
+                      }
+                    }
                   },
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  error,
+                  style: const TextStyle(color: Colors.red, fontSize: 14),
                 )
               ],
             ),
