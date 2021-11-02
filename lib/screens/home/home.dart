a0dff701ac1e39dc48db3a340a3ac695df735fb8
Ashraf Shah <ashrafashah@hotmail.com>
2021-11-02 01:44:31 -0600

    Firestore Streams

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,9 @@
+import 'package:brew_crew2/screens/home/brew_list.dart';
 import 'package:brew_crew2/services/auth.dart';
+import 'package:brew_crew2/services/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
@@ -7,22 +11,27 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: const Text('Brew Crew'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0,
-        actions: [
-          TextButton.icon(
-            icon: const Icon(Icons.person),
-            label: const Text('Logout'),
-            style: TextButton.styleFrom(primary: Colors.black),
-            onPressed: () async {
-              await _auth.signOut();
-            },
-          )
-        ],
+    return StreamProvider<QuerySnapshot?>.value(
+      value: DatabaseService(uid: '').brews,
+      initialData: null,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: const Text('Brew Crew'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0,
+          actions: [
+            TextButton.icon(
+              icon: const Icon(Icons.person),
+              label: const Text('Logout'),
+              style: TextButton.styleFrom(primary: Colors.black),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            )
+          ],
+        ),
+        body: const BrewList(),
       ),
     );
   }
