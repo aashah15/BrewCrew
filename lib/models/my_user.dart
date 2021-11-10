8381f9d40bbb867b6ac8b82473b6915cc58f3b10
Ashraf Shah <ashrafashah@hotmail.com>
2021-11-09 22:40:26 -0700

    User Data Model

--- lib/models/my_user.dart
+++ lib/models/my_user.dart
@@ -4,3 +4,16 @@ class MyUser {
     required this.uid,
   });
 }
+
+class UserData {
+  final String uid;
+  final String name;
+  final String sugars;
+  final int strength;
+
+  UserData(
+      {required this.uid,
+      required this.sugars,
+      required this.strength,
+      required this.name});
+}
