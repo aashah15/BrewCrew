770e882c17b996c8b3c7e86a3c66efc575bf0293
Ashraf Shah <ashrafashah@hotmail.com>
2021-11-11 15:42:49 -0700

    Using a Stream Builder

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,5 +1,9 @@
+import 'package:brew_crew2/models/my_user.dart';
+import 'package:brew_crew2/services/database.dart';
 import 'package:brew_crew2/shared/constants.dart';
+import 'package:brew_crew2/shared/loading.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   const SettingsForm({Key? key}) : super(key: key);
@@ -28,63 +32,81 @@ class _SettingsFormState extends State<SettingsForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          const Text(
-            'Update your brew settings.',
-            style: TextStyle(fontSize: 18.0),
-          ),
-          const SizedBox(height: 20.0),
-          TextFormField(
-            decoration: textInputDecoration,
-            validator: (val) => val!.isEmpty ? 'Please enter a name' : null,
-            onChanged: (val) => setState(() => _currentName = val),
-          ),
-          const SizedBox(height: 20.0),
-          // dropdown
-          DropdownButtonFormField(
-            value: _currentSugars == '' ? sugars[0] : sugars[0],
-            items: sugars.map((sugar) {
-              return DropdownMenuItem(
-                value: sugar,
-                child: Text('$sugar sugars'),
-              );
-            }).toList(),
-            // If we don't use onChanged, dropdown menu will not be clickable
-            onChanged: (val) {
-              setState(() {
-                _currentSugars = val.toString();
-              });
-            },
-          ),
-          // slider
-          Slider(
-            value:
-                (_currentStrength == 100 ? 100 : _currentStrength).toDouble(),
-            activeColor:
-                Colors.brown[_currentStrength == 100 ? 100 : _currentStrength],
-            inactiveColor:
-                Colors.brown[_currentStrength == 100 ? 100 : _currentStrength],
-            min: 100.0,
-            max: 900.0,
-            divisions: 8,
-            onChanged: (val) => setState(() => _currentStrength = val.round()),
-          ),
-          ElevatedButton(
-              child: const Text(
-                'Update',
-                style: TextStyle(color: Colors.white),
+    final user = Provider.of<MyUser?>(context);
+    return StreamBuilder<UserData>(
+        stream: DatabaseService(uid: user!.uid).userData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UserData? userData = snapshot.data;
+            return Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  const Text(
+                    'Update your brew settings.',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextFormField(
+                    initialValue: userData!.name,
+                    decoration: textInputDecoration,
+                    validator: (val) =>
+                        val!.isEmpty ? 'Please enter a name' : null,
+                    onChanged: (val) => setState(() => _currentName = val),
+                  ),
+                  const SizedBox(height: 20.0),
+                  // dropdown
+                  DropdownButtonFormField(
+                    value:
+                        _currentSugars == '' ? userData.sugars : _currentSugars,
+                    items: sugars.map((sugar) {
+                      return DropdownMenuItem(
+                        value: sugar,
+                        child: Text('$sugar sugars'),
+                      );
+                    }).toList(),
+                    // If we don't use onChanged, dropdown menu will not be clickable
+                    onChanged: (val) {
+                      setState(() {
+                        _currentSugars = val.toString();
+                      });
+                    },
+                  ),
+                  // slider
+                  Slider(
+                    value: (_currentStrength == 100
+                            ? userData.strength
+                            : _currentStrength)
+                        .toDouble(),
+                    activeColor: Colors.brown[_currentStrength == 100
+                        ? userData.strength
+                        : _currentStrength],
+                    inactiveColor: Colors.brown[_currentStrength == 100
+                        ? userData.strength
+                        : _currentStrength],
+                    min: 100.0,
+                    max: 900.0,
+                    divisions: 8,
+                    onChanged: (val) =>
+                        setState(() => _currentStrength = val.round()),
+                  ),
+                  ElevatedButton(
+                      child: const Text(
+                        'Update',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(primary: Colors.brown),
+                      onPressed: () async {
+                        print(_currentName);
+                        print(_currentSugars);
+                        print(_currentStrength);
+                      }),
+                ],
               ),
-              style: ElevatedButton.styleFrom(primary: Colors.brown),
-              onPressed: () async {
-                print(_currentName);
-                print(_currentSugars);
-                print(_currentStrength);
-              }),
-        ],
-      ),
-    );
+            );
+          } else {
+            return const Loading();
+          }
+        });
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew2/models/brew.dart';
+import 'package:brew_crew2/models/my_user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
@@ -31,5 +32,19 @@ class DatabaseService {
       return Brew(
           name: doc['name'], sugars: doc['sugars'], strength: doc['strength']);
     }).toList();
+  }
+
+  // user data from snapshots
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+        uid: uid!,
+        name: snapshot['name'],
+        sugars: snapshot['sugars'],
+        strength: snapshot['strength']);
+  }
+
+  // get user doc stream
+  Stream<UserData> get userData {
+    return brewCollection.doc(uid).snapshots().map(_userDataFromSnapshot);
   }
 }
