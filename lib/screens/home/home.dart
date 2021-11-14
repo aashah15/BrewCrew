a3dec254494fd1bce282d2a6a8b495d060172ac1
Ashraf Shah <ashrafashah@hotmail.com>
2021-11-13 21:13:02 -0700

    Finishing Touches

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -52,7 +52,12 @@ class Home extends StatelessWidget {
             )
           ],
         ),
-        body: const BrewList(),
+        body: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/coffee_bg.png'),
+                    fit: BoxFit.cover)),
+            child: const BrewList()),
       ),
     );
   }
